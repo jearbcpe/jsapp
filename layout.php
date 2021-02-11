3018ba86d6d3c8b351ad48d789bd13dffa6b7689
jearbcpe <teerapon.jearb@gmail.com>
2021-02-11 23:38:44 +0700

    first commit

---
+++ layout.php
@@ -0,0 +1,38 @@
+
+<!--begin::Main-->
+
+		<?php include("partials/_header-mobile.php"); ?>
+		<div class="d-flex flex-column flex-root">
+
+			<!--begin::Page-->
+			<div class="d-flex flex-row flex-column-fluid page">
+
+				<?php include("partials/_aside.php"); ?>
+
+				<!--begin::Wrapper-->
+				<div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+
+					<?php include("partials/_header.php"); ?>
+
+					<!--begin::Content-->
+					<div class="content d-flex flex-column flex-column-fluid" id="kt_content">
+
+						<?php include("partials/_subheader/subheader-v1.php"); ?>
+
+						<?php include("partials/_content.php"); ?>
+					</div>
+
+					<!--end::Content-->
+
+					<?php include("partials/_footer.php"); ?>
+				</div>
+
+				<!--end::Wrapper-->
+
+				<?php //include("partials/_sidebar.php"); ?>
+			</div>
+
+			<!--end::Page-->
+		</div>
+
+		<!--end::Main-->
